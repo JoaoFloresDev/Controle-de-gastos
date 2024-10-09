485577ea2ac5c973beb3e7954e2b89686836ce91
João Flores <joaoflores@MacBook-Pro-de-Joao.local>
2024-10-09 12:48:37 -0300

    aa

--- meus_gastos/lib/comprarPro/comprarPro.dart
+++ meus_gastos/lib/comprarPro/comprarPro.dart
@@ -1,27 +1,27 @@
-import "package:flutter_dotenv/flutter_dotenv.dart";
-import "package:http/http.dart";
+// import "package:flutter_dotenv/flutter_dotenv.dart";
+// import "package:http/http.dart";
 
-Future createPaymentIntent(
-    {required String name,
-    required String address,
-    required String pin,
-    required String city,
-    required String state,
-    required String country,
-    required String currency,
-    required String amount}) async {
-  // final url =
-  // final secretKey =
-  final body = {
-      "amount": amount,
-      "currency": currency.toLowerCase(),
-      // "automatic_payment_methods[enabled]"
-      "description": "payment for $name",
-      "shipping[name]": name,
-      "shipping[address][line1]": address,
-      "shipping[address][potal_code]": pin,
-      "shipping[address][city]": city,
-      "shipping[address][state]": state,
-      "shipping[address][country]": country
-  };
-}
+// Future createPaymentIntent(
+//     {required String name,
+//     required String address,
+//     required String pin,
+//     required String city,
+//     required String state,
+//     required String country,
+//     required String currency,
+//     required String amount}) async {
+//   // final url =
+//   // final secretKey =
+//   final body = {
+//       "amount": amount,
+//       "currency": currency.toLowerCase(),
+//       // "automatic_payment_methods[enabled]"
+//       "description": "payment for $name",
+//       "shipping[name]": name,
+//       "shipping[address][line1]": address,
+//       "shipping[address][potal_code]": pin,
+//       "shipping[address][city]": city,
+//       "shipping[address][state]": state,
+//       "shipping[address][country]": country
+//   };
+// }
